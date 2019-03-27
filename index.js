7d0476c25ecd1c7e2da406c963a28e0202e520a9
UnknownMonk <smodcartoon@gmail.com>
2019-03-27 17:55:45 -0400

    set up routes and seeds + migration

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
-// const cohortsRouts = require('./cohorts/cohorts-routes')
+const cohortsRouts = require('./cohorts/cohortsRoutes');
 
 const knexConfig = {
   client: 'sqlite3',
   connection: {
-    filename: './data/rolex.db3'
+    filename: './data/lambda.sqlite3'
   },
   useNullAsDefault: true // needed for sqlite
 };
@@ -16,6 +16,17 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use('/api/cohorts', cohortsRouts);
+
+server.get('/api/cohorts', (req, res) => {
+  db('cohorts')
+    .then(roles => {
+      res.status(200).json(roles);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Yeah Boi!! Server ${port} is running`));
